07382730f78c940db2b45bf3a3e97e8041b089ca
1277032935@qq.com <1277032935@qq.com>
2017-11-24 00:00:41 +0800

    1、添加banner文件；2、修改readme文件。

--- src/main/java/com/xwintop/xJavaFxTool/Main.java
+++ src/main/java/com/xwintop/xJavaFxTool/Main.java
@@ -1,17 +1,13 @@
 package com.xwintop.xJavaFxTool;
 
 import com.xwintop.xJavaFxTool.fxmlView.IndexView;
-import com.xwintop.xJavaFxTool.utils.Config;
 import com.xwintop.xJavaFxTool.utils.XJavaFxSystemUtil;
-import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
-import javafx.application.Platform;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
+import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
 
 /** 
  * @ClassName: Main 
@@ -27,15 +23,15 @@ public class Main extends AbstractJavaFxApplicationSupport {
 		PropertyConfigurator.configure(Main.class.getResource("/config/log4j.properties"));//加载日志配置
 		XJavaFxSystemUtil.initSystemLocal();//初始化本地语言
 		XJavaFxSystemUtil.addJarByLibs();//添加外部jar包
-		new Timer().schedule(new TimerTask() {
-			@Override
-			public void run() {
-				Platform.runLater(()->{
-					ResourceBundle resourceBundle = ResourceBundle.getBundle("locale.Menu", Config.defaultLocale);
-					AbstractJavaFxApplicationSupport.setTitle(resourceBundle.getString("Title"));
-				});
-			}
-		},5000);
+//		new Timer().schedule(new TimerTask() {
+//			@Override
+//			public void run() {
+//				Platform.runLater(()->{
+//					ResourceBundle resourceBundle = ResourceBundle.getBundle("locale.Menu", Config.defaultLocale);
+//					AbstractJavaFxApplicationSupport.setTitle(resourceBundle.getString("Title"));
+//				});
+//			}
+//		},5000);
 
 		launchApp(Main.class, IndexView.class, args);
 	}
--- src/main/java/com/xwintop/xJavaFxTool/fxmlView/IndexView.java
+++ src/main/java/com/xwintop/xJavaFxTool/fxmlView/IndexView.java
@@ -1,14 +1,16 @@
 package com.xwintop.xJavaFxTool.fxmlView;
 
 import com.xwintop.xJavaFxTool.utils.Config;
-import de.felixroske.jfxsupport.AbstractFxmlView;
-import de.felixroske.jfxsupport.FXMLView;
+
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.context.annotation.Scope;
 
-import java.util.Locale;
 import java.util.Optional;
 import java.util.ResourceBundle;
+
+import de.felixroske.jfxsupport.AbstractFxmlView;
+import de.felixroske.jfxsupport.FXMLView;
+import de.felixroske.jfxsupport.GUIState;
 
 /** 
  * @ClassName: IndexView
@@ -23,5 +25,6 @@ public class IndexView extends AbstractFxmlView {
         //反射修改默认语言
         ResourceBundle bundle = ResourceBundle.getBundle(this.getResourceBundle().get().getBaseBundleName(), Config.defaultLocale);
         FieldUtils.writeField(this,"bundle",Optional.ofNullable(bundle),true);
+        GUIState.getStage().setTitle(bundle.getString("Title"));//修改标题国际化
     }
 }
